18857fbb091c77a900f8c64eb244988acc1996b8
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-19 23:49:37 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('create page', () => {
+  render(<App />);
+  const CreateElement = screen.getElement(/Create);
+  expect(CreateElement).toBeInTheDocument();
+});
