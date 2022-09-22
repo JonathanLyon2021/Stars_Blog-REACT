54adf13913979e085fd6ac806b387202cffb3c23
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-21 23:56:29 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -21,19 +21,16 @@ test('Registration page', () => {
   const RegistrationElement = screen.getElement(/Registiration);
   expect(RegistrationElement).toBeInTheDocument();
 });
-
 test('Login page', () => {
   render(<App />);
   const LoginElement = screen.getElement(/Login);
   expect(LoginElement).toBeInTheDocument();
 });
-
 test('Blog Details page', () => {
   render(<App />);
   const BlogDetailsElement = screen.getElement(/BlogDetails);
   expect(BlogDetailsElement).toBeInTheDocument();
 });
-
 test('Not Found page', () => {
   render(<App />);
   const NotFoundElement = screen.getElement(/NotFound);
